176ecc8036a3c8edb2352c33314509fc729a3295
Jordan Harband <ljharb@gmail.com>
2022-05-23 16:06:21 -0700

    [Tests] update TODO mechanism

--- test/preprocessor.js
+++ test/preprocessor.js
@@ -6,6 +6,11 @@ try {
 	hasArrows = true;
 } catch (e) { /**/ }
 
+// TODO: remove anything annotated with this
+function TODO(condition) {
+	return !process.env.RUN_TODOS && condition;
+}
+
 function hasUnsupportedAttrs(test) {
 	if (typeof Symbol !== 'function') {
 		if (
@@ -28,13 +33,13 @@ module.exports = function (test) {
 	if (test && test.attrs) {
 		if (
 			test.attrs.esid && (
-				test.attrs.esid.startsWith('sec-date-') // TODO: remove
-				|| test.attrs.esid.startsWith('sec-date.') // TODO: remove
-				|| test.attrs.esid === 'sec-expanded-years' // TODO: remove
-				|| test.attrs.esid === 'sec-constructor-properties-of-the-global-object-date' // TODO: remove
-				|| test.attrs.esid === 'sec-aggregate-error.prototype.constructor' // TODO: remove
-				|| test.attrs.esid === 'sec-regexp.prototype-@@replace' // TODO: remove
-				|| test.attrs.esid === 'sec-symbol.prototype-@@toprimitive' // TODO: remove
+				TODO(test.attrs.esid.startsWith('sec-date-'))
+				|| TODO(test.attrs.esid.startsWith('sec-date.'))
+				|| TODO(test.attrs.esid === 'sec-expanded-years')
+				|| TODO(test.attrs.esid === 'sec-constructor-properties-of-the-global-object-date')
+				|| TODO(test.attrs.esid === 'sec-aggregate-error.prototype.constructor')
+				|| TODO(test.attrs.esid === 'sec-regexp.prototype-@@replace')
+				|| TODO(test.attrs.esid === 'sec-symbol.prototype-@@toprimitive')
 				|| test.attrs.esid === 'sec-decodeuri-encodeduri'
 				|| test.attrs.esid === 'sec-decodeuricomponent-encodeduricomponent'
 				|| test.attrs.esid === 'sec-invariants-of-the-essential-internal-methods'
@@ -46,12 +51,12 @@ module.exports = function (test) {
 		if (
 			test.attrs.es5id
 			&& (
-				test.attrs.es5id === '10.2.3_A2.1_T3' // TODO: non-enumerable globals; "Date"
-				|| test.attrs.es5id === '10.2.3_A2.3_T3' // TODO: non-enumerable globals; "Date"
-				|| test.attrs.es5id === '15.2.3.3-4-210' // TODO: Date.prototype descriptor
-				|| test.attrs.es5id === '15.2.3.14-2-3' // TODO: weird Object.keys failure
-				|| test.attrs.es5id === '15.2.3.7-6-a-24' // TODO: weird Object.defineProperties failure
-				|| test.attrs.es5id === '15.2.3.7-2-18' // TODO: weird Object.defineProperties failure
+				TODO(test.attrs.es5id === '10.2.3_A2.1_T3') // TODO: non-enumerable globals; "Date"
+				|| TODO(test.attrs.es5id === '10.2.3_A2.3_T3') // TODO: non-enumerable globals; "Date"
+				|| TODO(test.attrs.es5id === '15.2.3.3-4-210') // TODO: Date.prototype descriptor
+				|| TODO(test.attrs.es5id === '15.2.3.14-2-3') // TODO: weird Object.keys failure
+				|| TODO(test.attrs.es5id === '15.2.3.7-6-a-24') // TODO: weird Object.defineProperties failure
+				|| TODO(test.attrs.es5id === '15.2.3.7-2-18') // TODO: weird Object.defineProperties failure
 			)
 		) {
 			return null;
@@ -59,7 +64,7 @@ module.exports = function (test) {
 
 		if (
 			test.attrs.description
-			&& test.attrs.description.startsWith('Checking parseInt.prototype')
+			&& TODO(test.attrs.description.startsWith('Checking parseInt.prototype'))
 		) { // TODO: investigate using Function.call in parseInt shim
 			return null;
 		}
@@ -77,7 +82,7 @@ module.exports = function (test) {
 		if (
 			test.attrs.features
 			&& (
-				test.attrs.features.indexOf('Reflect.construct') > -1 // TODO: investigate using Function.call in parseInt shim
+				TODO(test.attrs.features.indexOf('Reflect.construct') > -1) // TODO: investigate using Function.call in parseInt shim
 				|| test.attrs.features.indexOf('resizable-arraybuffer') > -1
 				|| test.attrs.features.indexOf('regexp-unicode-property-escapes') > -1
 				|| test.attrs.features.indexOf('regexp-named-groups') > -1
